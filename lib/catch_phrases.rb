49ceb6ad7cc3343c05510b4b611ad635eff48c5a
Marius Phoenix <phoenixzenomarius@gmail.com>
2019-12-20 16:43:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+<<<<<<< HEAD
   phrase = "It's-a me, Mario!"
+=======
+  phrase = 
+>>>>>>> 860d11ed59183bd7d89f82aae237f10b92867f07
   puts phrase
 end
 
